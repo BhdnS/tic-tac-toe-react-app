d3915d9780a27fbf4742782807765da38193a26b
Bohdan <boghdan.shimko@gmail.com>
2024-01-12 23:10:48 +0200

    add: dist

--- src/components/Layout.jsx
+++
@@ -1,5 +0,0 @@
-const Layout = ({ children }) => {
-  return <div className='game'>{children}</div>
-}
-
-export default Layout
--- src/utils/calculateRowCol.js
+++
@@ -1,7 +0,0 @@
-const calculateRowCol = (item) => {
-  const row = Math.floor((item - 1) / 3) + 1
-  const col = ((item - 1) % 3) + 1
-  return { row, col }
-}
-
-export default calculateRowCol
